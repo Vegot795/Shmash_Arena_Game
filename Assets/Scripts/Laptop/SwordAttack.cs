8701025ed0d9665db7c6492387b39f55ab9383a2
Vegot795 <167176338+Vegot795@users.noreply.github.com>
2024-08-20 11:42:33 +0200

    finalMerge

---
+++ Assets/Scripts/Laptop/SwordAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    public Collider2D swordCollider;
+    public float damage = 3;
+    Vector2 rightAttackOffset;
+
+    private void Start() {
+        rightAttackOffset = transform.position;
+    }
+
+    public void AttackRight() {
+        swordCollider.enabled = true;
+        transform.localPosition = rightAttackOffset;
+        print("attack right");
+    }
+
+    public void AttackLeft() {
+        swordCollider.enabled = true;
+        transform.localPosition = new Vector2(rightAttackOffset.x * -1, rightAttackOffset.y);
+        print("attack left");
+    }
+
+    public void StopAttack() {
+        swordCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Enemy") {
+            // Deal damage to the enemy
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if(enemy != null) {
+                enemy.health -= damage;
+                print("enemy found");
+            } else
+            {
+                print("missed");
+            }
+        } else
+        {
+            print("missed");
+        }
+    }
+}
